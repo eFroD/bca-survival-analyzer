a5e82242d517c1abfe17550ae4cc5ff4b53c6dee
Eric Frodl <frodl@med.uni-frankfurt.de>
2024-06-03 16:02:40 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bca_survival_analyzer',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "lifelines",
+        "scikit-learn"
+    ],
+    entry_points={
+        'console_scripts': [
+            # If your package includes scripts, add them here
+        ],
+    },
+)
