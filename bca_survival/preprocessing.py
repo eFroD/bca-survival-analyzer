640c30a96d9ee58bb3c41d53d5f422bbe5d7f256
Eric Frodl <frodl@med.uni-frankfurt.de>
2024-07-17 16:59:13 +0200

    add computation of ratios

--- bca_survival/preprocessing.py
+++ bca_survival/preprocessing.py
@@ -34,3 +34,35 @@ def create_event_date_column(df, date_death, date_disease_death, date_followup):
             df.loc[i, "event_date"] = pd.NA
             df.loc[i, "event"] = pd.NA
     return df
+
+
+def compute_ratios(df):
+    # Define the parts and metrics
+    body_parts = ['ventral_cavity', 'abdominal_cavity', 'thoracic_cavity', 'mediastinum', 'pericardium', 'l5', 'l4',
+                  'l3', 'l2', 'l1', 't12', 't11', 't10', 't9', 't8', 't7', 't6', 't5', 't4', 't3', 't2', 't1']
+    metrics = [
+        "mean_ml",
+        "std_ml",
+        "min_ml",
+        "q1_ml",
+        "q2_ml",
+        "q3_ml",
+        "max_ml",
+        "sum_ml",
+        "mean_hu"
+    ]
+
+    ratios = [('imat', 'tat'), ('vat', 'tat'), ('eat', 'tat'), ('sat', 'tat'), ('pat', 'tat'), ('muscle', 'bone')]
+
+    # Iterate through each body part and metric combination
+    for body_part in body_parts:
+        for metric in metrics:
+            for (numerator, denominator) in ratios:
+                num_col = f'{body_part}::WL::{numerator}::{metric}'
+                den_col = f'{body_part}::WL::{denominator}::{metric}'
+                new_col = f'{body_part}::WL::{numerator}/{denominator}::{metric}'
+
+                if num_col in df.columns and den_col in df.columns:
+                    df[new_col] = df[num_col] / df[den_col]
+
+    return df
